f2132cc2cf72f9408fbce2a20b5a21999f1e9416
Joey Parrish <joeyparrish@google.com>
2024-05-22 21:47:10 +0000

    fix: Fix compatibility with Tizen WebDriver Server and useIframe: false
    Running without iframes in Karma still involves an iframe in Tizen WebDriver Server.  Karma must not assume that the existence of a parent window means that parent window is Karma.

--- context/main.js
+++ context/main.js
@@ -1,20 +1,25 @@
 // Load in our dependencies
 var ContextKarma = require('./karma')
 
-// Resolve our parent window
-var parentWindow = window.opener || window.parent
+if (!window.karma) {
+  // Resolve our parent window
+  var parentWindow = window.opener || window.parent
 
-// Define a remote call method for Karma
-var callParentKarmaMethod = ContextKarma.getDirectCallParentKarmaMethod(parentWindow)
+  // Define a remote call method for Karma
+  callParentKarmaMethod = ContextKarma.getDirectCallParentKarmaMethod(parentWindow)
 
-// If we don't have access to the window, then use `postMessage`
-// DEV: In Electron, we don't have access to the parent window due to it being in a separate process
-// DEV: We avoid using this in Internet Explorer as they only support strings
-//   https://caniuse.com/?search=postmessage
-var haveParentAccess = false
-try { haveParentAccess = !!parentWindow.window } catch (err) { /* Ignore errors (likely permission errors) */ }
-if (!haveParentAccess) {
-  callParentKarmaMethod = ContextKarma.getPostMessageCallParentKarmaMethod(parentWindow)
+  // If we don't have access to the window, then use `postMessage`
+  // DEV: In Electron, we don't have access to the parent window due to it being in a separate process
+  // DEV: We avoid using this in Internet Explorer as they only support strings
+  //   https://caniuse.com/?search=postmessage
+  var haveParentAccess = false
+  try { haveParentAccess = !!parentWindow.window } catch (err) { /* Ignore errors (likely permission errors) */ }
+  if (!haveParentAccess) {
+    callParentKarmaMethod = ContextKarma.getPostMessageCallParentKarmaMethod(parentWindow)
+  }
+} else {
+  // Everything is in the same window
+  callParentKarmaMethod = ContextKarma.getDirectCallParentKarmaMethod(window)
 }
 
 // Define a window-scoped Karma
--- static/context.js
+++ static/context.js
@@ -301,20 +301,27 @@ module.exports = ContextKarma
 // Load in our dependencies
 var ContextKarma = require('./karma')
 
-// Resolve our parent window
-var parentWindow = window.opener || window.parent
+var callParentKarmaMethod
 
-// Define a remote call method for Karma
-var callParentKarmaMethod = ContextKarma.getDirectCallParentKarmaMethod(parentWindow)
+if (!window.karma) {
+  // Resolve our parent window
+  var parentWindow = window.opener || window.parent
 
-// If we don't have access to the window, then use `postMessage`
-// DEV: In Electron, we don't have access to the parent window due to it being in a separate process
-// DEV: We avoid using this in Internet Explorer as they only support strings
-//   https://caniuse.com/?search=postmessage
-var haveParentAccess = false
-try { haveParentAccess = !!parentWindow.window } catch (err) { /* Ignore errors (likely permission errors) */ }
-if (!haveParentAccess) {
-  callParentKarmaMethod = ContextKarma.getPostMessageCallParentKarmaMethod(parentWindow)
+  // Define a remote call method for Karma
+  callParentKarmaMethod = ContextKarma.getDirectCallParentKarmaMethod(parentWindow)
+
+  // If we don't have access to the window, then use `postMessage`
+  // DEV: In Electron, we don't have access to the parent window due to it being in a separate process
+  // DEV: We avoid using this in Internet Explorer as they only support strings
+  //   https://caniuse.com/?search=postmessage
+  var haveParentAccess = false
+  try { haveParentAccess = !!parentWindow.window } catch (err) { /* Ignore errors (likely permission errors) */ }
+  if (!haveParentAccess) {
+    callParentKarmaMethod = ContextKarma.getPostMessageCallParentKarmaMethod(parentWindow)
+  }
+} else {
+  // Everything is in the same window
+  callParentKarmaMethod = ContextKarma.getDirectCallParentKarmaMethod(window)
 }
 
 // Define a window-scoped Karma
